b5fab2aa2ca8ebd6f2c05963e1a7f66d877c1fef
larissasilabi <larissa@softclass.com.br>
2017-05-05 06:57:05 -0300

    jj

--- src/app/principal/perguntas/perguntas.component.ts
+++ src/app/principal/perguntas/perguntas.component.ts
@@ -1,3 +1,5 @@
+import { routing } from './../../app.routing';
+import { Router, ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerguntasComponent implements OnInit {
 
-  constructor() { }
+  id: number = 1;
+
+  perguntas: string[];
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+
+    this.perguntas[0] = "O que é o serviço de AWS da Amazon?1";
+    this.perguntas[1] = "O que é o serviço de AWS da Amazon?2";
+    this.perguntas[2] = "O que é o serviço de AWS da Amazon?3";
+    this.perguntas[3] = "O que é o serviço de AWS da Amazon?4";
+    this.perguntas[4] = "O que é o serviço de AWS da Amazon?5";
+    this.perguntas[5] = "O que é o serviço de AWS da Amazon?6";
+
+    //this.id = this.route.params['id'];
+    console.log(this.route.params['id']);
+  }
+
+  proximaPergunta() {
+    this.router.navigate(['/perguntas'], [this.id + 1]);
   }
 
 }
