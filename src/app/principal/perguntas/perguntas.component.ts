00db7062cd6eb5f2b4f0c5df7f373310a91b0150
Larissa <larissa@softclass.com.br>
2017-05-10 14:35:46 -0300

    .

--- src/app/principal/perguntas/perguntas.component.ts
+++ src/app/principal/perguntas/perguntas.component.ts
@@ -46,44 +46,74 @@ export class PerguntasComponent implements OnInit {
   }
 
   teste() {
-    var $credentials = {
-      "accessKeyId": "AKIAITVF5OWLUUUAAOYA",
-      "secretAccessKey": "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",
-      "region": "ap-southeast-2"
-    }
-    var AWS = require('aws-sdk');
-    var $db = new AWS.DynamoDB();
-    var DynamoDB = require('@awspilot/dynamodb')($db);
-
-    DynamoDB
-      .table('users')
-      .insert({
-        id: 'teste',
-        acertos: 1,
-      }, function (err, data) {
-        console.log(err, data)
-      })
-
+    // var $credentials = {
+    //   "accessKeyId": "AKIAITVF5OWLUUUAAOYA",
+    //   "secretAccessKey": "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",
+    //   "region": "sa-east-1"
+    // }
     // var AWS = require('aws-sdk');
-    // var s3 = new AWS.S3();
-    // // Os nomes do bucket devem ser exclusivos em todos os usuários do S3
-    // var myBucket = 'my.unique.bucket.name';
-    // var myKey = 'myBucketKey';
-    // s3.createBucket({ Bucket: myBucket }, function (err, data) {
-    //   if (err) {
-    //     console.log(err);
-    //   } else {
-    //     params = { Bucket: myBucket, Key: myKey, Body: 'Hello!' };
-    //     s3.putObject(params, function (err, data) {
-    //       if (err) {
-    //         console.log(err)
-    //       } else {
-    //         console.log("Dados enviados com sucesso para myBucket/myKey");
-    //       }
-    //     });
-    //   }
-    // });
-  }
+    // var $db = new AWS.DynamoDB();
+    // var DynamoDB = require('@awspilot/dynamodb')($db);
 
+    // DynamoDB
+    //   .table('users')
+    //   .insert({
+    //     id: 'teste',
+    //     acertos: 1,
+    //   }, function (err, data) {
+    //     console.log(err, data)
+    //   })
+
+    var AWS = require('aws-sdk');
+    AWS.config = new AWS.Config();
+    AWS.config.accessKeyId = "AKIAITVF5OWLUUUAAOYA";
+    AWS.config.secretAccessKey = "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";
+    AWS.config.region = "sa-east-1";
+    var s3 = new AWS.S3();
+    // Os nomes do bucket devem ser exclusivos em todos os usuários do S3
+    var myBucket = '953382053291';
+    var myKey = 'myBucketKey';
+    var params = {
+      Key: { HashKeyElement: {}},
+      TableName: 'users'
+    };
+
+    s3.createBucket({ Bucket: myBucket }, function (err, data) {
+      if (err) {
+        console.log(err);
+      } else {
+        s3.putObject(params, function (err, data) {
+          if (err) {
+            console.log(err)
+          } else {
+            console.log("Dados enviados com sucesso para myBucket/myKey");
+          }
+        });
+      }
+    });
+
+
+    // var AWS = require("aws-sdk");
+
+    // AWS.config = new AWS.Config();
+    // AWS.config.accessKeyId = "AKIAITVF5OWLUUUAAOYA";
+    // AWS.config.secretAccessKey = "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";
+    // AWS.config.region = "sa-east-1";
+
+    // var dynamodb = new AWS.DynamoDB({ apiVersion: '2011-12-05' });
+    // var docClient = new AWS.DynamoDB.DocumentClient();
+
+    // console.log("Querying for users.");
+
+    // var params = {
+    //   Key: { HashKeyElement: {}},
+    //   TableName: 'users'
+    // };
+    // dynamodb.getItem(params, function (err, data) {
+    //   if (err) console.log(err, err.stack); // an error occurred
+    //   else console.log(data);           // successful response
+    // });
+
+  }
 
 }
