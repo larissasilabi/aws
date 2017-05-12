a24c69e6b4d5eafb6c00965404a727640c2c75ff
larissasilabi <larissa@softclass.com.br>
2017-05-12 17:36:47 -0300

    .

--- src/app/principal/perguntas/perguntas.service.ts
+++ src/app/principal/perguntas/perguntas.service.ts
@@ -6,7 +6,7 @@ export class PerguntasService {
 
   private perguntas: Pergunta[] = [
     {id: 1, texto: '1) O que motivou a Amazon a criar o produto AWS (Amazon Web Services) ?', a: 'Oportunidade de mercado', b: 'Expandir áreas de atuação', c: 'Necessidades próprias', d: 'Para ser o produto principal da empresa', correta: 'c'},
-    {id: 2, texto: '2) Qual a principal característica do produto EC2 da Amazon?', a: 'Elasticidade automática', b: 'Servidores virtuais na nuvem', c: 'Armazenamento escalável em nuvem', d: 'Banco de daod NoSQL gerenciado', correta: 'b'},
+    {id: 2, texto: '2) Qual a principal característica do produto EC2 da Amazon?', a: 'Elasticidade automática', b: 'Servidores virtuais na nuvem', c: 'Armazenamento escalável em nuvem', d: 'Banco de dados NoSQL gerenciado', correta: 'b'},
     {id: 3, texto: '3) Em qual dos seguimentos o AWS não atua ?', a: 'Saúde e ciências biológicas', b: 'Serviços financeiros', c: 'Internet das coisas (IoT)', d: 'Nenhuma das opções', correta: 'd'},
     {id: 4, texto: '4) A partir de qual plano de suporte está incluso atendimento online?', a: 'Business', b: 'Basic', c: 'Enterprise', d: 'Desenvolvedor', correta: 'a'},
     {id: 5, texto: '5) Quais dos modelos de computação em nuvem tem um crescimento maior ?', a: 'PaaS e SaaS', b: 'SaaS e IaaS', c: 'IaaS e PaaS', d: 'Todos estão crescendo igualmente', correta: 'c'},
